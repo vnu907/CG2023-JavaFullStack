1be13b1be6e9874a43898c6c7f5fe168b825a4b8
vnu907 <vnu0212@gmail.com>
2023-04-02 17:02:30 +0530

    Adding airlines folder

---
+++ Filght Reservation System/Airlines/src/main/java/Flight/booking/system/Airlines/AirlinesApplication.java
@@ -0,0 +1,13 @@
+package Flight.booking.system.Airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesApplication.class, args);
+	}
+
+}
---
+++ Filght Reservation System/Airlines/src/test/java/Flight/booking/system/Airlines/AirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package Flight.booking.system.Airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
